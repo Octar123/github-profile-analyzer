7207d48e3c6e4a14eba4a9c15f2e6abc049096e5
Octar123 <ranjanassmali123@gmail.com>
2026-07-05 20:06:23 +0530

    feat: add index.html for API reference and update server.js to serve static files

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ import express from 'express';
 import { initializeDB } from './config/initDb.js';
 import * as dotenv from 'dotenv';
 import profileRoutes from './routes/profileRoutes.js'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 dotenv.config();
 
@@ -12,12 +17,7 @@ const startServer = async () => {
     await initializeDB();
 
     app.use("/api/v1", profileRoutes);
-    app.use("/", (req, res) => {
-        res.send("hi");
-    })
-
-
-
+    
     const PORT = process.env.PORT || 3000;
 
     app.listen(PORT, () => {
